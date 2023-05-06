78f5a638e1eb5a510fe8f09ef1135d21299ff81b
Leul Sileshi <106759704+Lulusha-XT@users.noreply.github.com>
2023-05-06 14:30:48 +0300

    final temp

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ app.use("uploads", express.static(path.resolve("uploads")));
 // Server Starter
 (async () => {
   try {
-    await connectDB();
+    // await connectDB();
     app.listen(app.get("port"));
     console.log(
       `Server is running on port http://localhost:${app.get("port")}`
